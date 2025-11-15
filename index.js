535ece1b7b6d898c525108ec76aa0259c23564fa
Rpenubarthi <penubarthi.r@northeastern.edu>
2025-11-15 15:55:25 -0500

    Third

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ import Lab5 from "./Lab5/index.js";
 import cors from 'cors';
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
-
+import "dotenv/config";
+import session from "express-session";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
 
 
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+})
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.SERVER_URL,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
-Lab5(app);
 UserRoutes(app, db);
+CourseRoutes(app, db);
+Lab5(app);
 app.listen(process.env.PORT || 4000);
