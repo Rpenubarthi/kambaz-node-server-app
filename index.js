2b4714b16be3e5c5f298ef2ce73f944e710be221
Ruthvik Penubarthi <102499197+Rpenubarthi@users.noreply.github.com>
2025-11-30 17:39:40 -0500

    A6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from "mongoose";
 import Lab5 from "./Lab5/index.js";
 import cors from 'cors';
 import db from "./Kambaz/Database/index.js";
@@ -9,7 +10,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 
 
 
-
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
